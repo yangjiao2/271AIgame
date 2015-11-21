ba521d5d39f824f00c35f658ca87b789e28c87e1
martinisawesome <mmao@uci.edu>
2015-11-20 18:26:39 -0800

    new cost function

--- CS271 Project-1/CostVs.py
+++ CS271 Project-1/CostVs.py
@@ -20,30 +20,26 @@ class CostFunction(object):
 		self.marker = marker
 		self.board  = board
 		self.boardLength = length
-		self.max = [[0] * self.boardLength for i in range(self.boardLength)]
-		self.min = [[0] * self.boardLength for i in range(self.boardLength)]
+		self.tiles = [[0] * self.boardLength for i in range(self.boardLength)]
 		for i in range(0, self.boardLength):
 			for j in range(0, self.boardLength):
 			
 				#My piece on board
 				if self.board[i][j] == marker:
-					self.max[i][j] = illegalSpace
-					self.min[i][j] = illegalSpace
+					self.tiles[i][j] = illegalSpace
 					self.upMaxValues(i, j, 1)
 				
 				#Empty Space on board
 				elif self.board[i][j] == NONE:
 				
 					#Empty Space not on sides of board is better
-					if (i > 0) and i < (self.boardLength - 1):
-						if (j > 0) and j < (self.boardLength - 1):
-							self.max[i][j] = self.max[i][j] + 1
-							self.min[i][j] = self.min[i][j] + 1
+					#if (i > 0) and i < (self.boardLength - 1):
+					#	if (j > 0) and j < (self.boardLength - 1):
+					#		self.tiles[i][j] = self.tiles[i][j] + 1
 				
 				#Opp space on board
 				else:
-					self.max[i][j] = illegalSpace
-					self.min[i][j] = illegalSpace
+					self.tiles[i][j] = illegalSpace
 					self.upMinValues(i, j, 1)
 					
 		self.bestX = -1
@@ -58,55 +54,45 @@ class CostFunction(object):
 #=======================================================================					
 	#Make quick adjustment to board for 1 single move
 	def makeMove(maxOrMin, i, j):
-		self.max[i][j] = illegalSpace
-		self.min[i][j] = illegalSpace
+		self.tiles[i][j] = illegalSpace
 		if maxOrMin:
 			self.upMaxValues(i, j, 1)
 		else:
 			self.upMinValues(i, j, 1)
 			
-		self.leastMax = _findLowerBoundValue(self.max)
-		self.leastMin = _findLowerBoundValue(self.min)	
+		self.leastMax = _findLowerBoundValue(True)
+		self.leastMin = _findLowerBoundValue(False)
 	
 	def findBestQuickMove(self):
-		#First see if min can win...
-		bestMin = self.findBestQuickValue(self.min)
-		bestMinX = self.bestX
-		bestMinY = self.bestY
-		
-		#See if max can win
-		bestMax = self.findBestQuickValue(self.max)
-		
-		#If min can win soon, try to block
-		if bestMin >= MOVES_TO_WIN and bestMin > bestMax:
-			self.bestX = bestMinX
-			self.bestY = bestMinY
-		#Otherwise, default will keep Max Best
+		self._findBestQuickValue(True)
 
 	def findBestMove(self, maxOrMin):
 		if maxOrMin:
-			return findBestQuickValue (self.max)
+			return _findBestQuickValue(True)
 		else:
-			return findBestQuickValue (self.min)
+			return _findBestQuickValue(False)
 	
-	def findBestQuickValue(self, matrix):
-		bestValue = -1
+	def _findBestQuickValue(self, maxOrMin):
+		if maxOrMin:
+			bestValue = -1
+		else:
+			bestValue = 1
 		
 		for i in range(0, self.boardLength):
 			for j in range(0, self.boardLength):
-				if matrix[i][j] == illegalSpace:
+				if self.tiles[i][j] == illegalSpace:
 					continue
 					
-				absValue = abs(matrix[i][j])
+				value = self.tiles[i][j]
 
-				if absValue > bestValue:
-					bestValue = absValue
+				if (maxOrMin and value > bestValue) or (not maxOrMin and value < bestValue):
+					bestValue = value
 					
 					#Start a new array of best moves
 					xList = []
 					yList = []
 				
-				if absValue >= bestValue:
+				if (maxOrMin and value >= bestValue) or (not maxOrMin and value <= bestValue):
 					xList.append(i)
 					yList.append(j)
 		
@@ -119,24 +105,33 @@ class CostFunction(object):
 		
 	#Used to find the min value to search in branching
 	#will not open nodes below a threshold
-	def _findLowerBoundValue(self, matrix):
+	def _findLowerBoundValue(self, maxOrMin):
 		global maxBranchingFactor
 		valueList = [0] * maxBranchingFactor
 		
 		for i in range(0, self.boardLength):
 			for j in range(0, self.boardLength):
-				if matrix[i][j] == illegalSpace:
+				if self.tiles[i][j] == illegalSpace:
 					continue
 					
-				absValue = abs(matrix[i][j])
+				value = self.tiles[i][j]
 
-				#replace min value
-				min = findMin(valueList)
-				if absValue > min:
-					_replaceMin(valueList, min, absValue)
+				if maxOrMin:
+					#replace min value
+					min = _findMin(valueList)
+					if value > min:
+						_replace(valueList, min, value)
+				#Replace max value
+				else:
+					max = _findMax(valueList)
+					if value < max:
+						_replace(valueList, max, value)
 		
 		#Return the lowest acceptable value
-		return _findMin(valueList)
+		if maxOrMin:
+			return _findMin(valueList)
+		else:
+			return _findMax(valueList)	
 
 	def _findMin(self, valueList):
 		min  = 999
@@ -145,26 +140,39 @@ class CostFunction(object):
 				min = valueList[i]
 		return min
 
-	def _replaceMin(self, valueList, oldValue, newValue):
+	def _replace(self, valueList, oldValue, newValue):
 		for i in range(0, len(valueList)):
 			if valueList[i] == oldValue:
 				valueList[i] = newValue
 				break
-	
+				
+	def _findMax(self, valueList):
+		max  = -999
+		for i in range(0, len(valueList)):
+			if valueList[i] > max:
+				max = valueList[i]
+		return max
+
 	def close(self):
-		self.max = -1
-		self.min = -1
+		self.tiles = -1
 					
 #=======================================================================
 #	PRIVATE METHOD TO CHANGE COST FUNCTIONS
 #=======================================================================			
 	def upMaxValues(self, x, y, value):
-		self.upValues(x, y, value, self.max)
+		self.upValues(x, y, value, True)
 					
 	def upMinValues(self, x, y, value):
-		self.upValues(x, y, value, self.min)
+		self.upValues(x, y, value, False)
 					
-	def upValues(self, x, y, value, matrix):
+	def upValues(self, x, y, value, maxOrMin):
+		if maxOrMin:
+			mod = value
+		else:
+			mod = value * -1
+		
+		matrix = self.tiles
+		
 		for i in range(0, self.boardLength):
 			for j in range(0, boardLength):
 				if matrix[i][j] == illegalSpace:
@@ -175,13 +183,13 @@ class CostFunction(object):
 			
 				#Same Row
 				if yDiff == 0 and xDiff < MOVES_TO_WIN:
-						matrix[i][j] = matrix[i][j] + value
+						matrix[i][j] = matrix[i][j] + mod
 				
 				#Same Column
 				if xDiff == 0 and yDiff < MOVES_TO_WIN:
-						matrix[i][j] = matrix[i][j] + value		
+						matrix[i][j] = matrix[i][j] + mod		
 						
 				#Same Diagonal
 				if xDiff == yDiff and yDiff < MOVES_TO_WIN:
-					matrix[i][j] = matrix[i][j] + value				
+					matrix[i][j] = matrix[i][j] + mod				
 					
